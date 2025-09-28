371b076e5fd9c8b733c502e024aa74500e72869c
Grace-Rasaily780 <rasailygrace56@gmail.com>
2025-09-28 12:04:25 +0545

    basic layout

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [mode, setMode] = useState<"noob" | "smiley" | "nerd" | "beast">("noob");
+  const [title, setTitle] = useState<string>("");
+
+  useEffect(() => {
+    const getId = async () => {
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      setTitle(tab.title);
+    }
+    getId();
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="field">
+        <label>Mode</label>
+        <select name="mode" id="mode" onChange={(e) => {setMode(e.target.value);}}
+        >
+          <option value="noob">Noob</option>
+          <option value="smiley">Smiley</option>
+          <option value="nerd">Nerd</option>
+          <option value="beast">Beast</option>
+        </select>
       </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="field">
+        <label>Webpage</label>
+        <span>{title}</span>
       </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+
+      <button>
+        Convert 🔄
+      </button>
+    </div>
   );
 }
 
